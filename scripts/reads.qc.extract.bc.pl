2cc0e44f48238510949d9c1d81ab7bfa4138c777
JackJRiley <jackjriley1@gmail.com>
2025-06-23 09:20:16 +0100

    fixed bug in barcode extraction script

--- scripts/reads.qc.extract.bc.pl
+++ scripts/reads.qc.extract.bc.pl
@@ -18,9 +18,9 @@ while(<IN>){
 	my @sp = split/:/, $tmp[3]; 
 #	my $l = length($sp);
 #	my $read_id = substr($sp, 0, $l-18);
-	my $cell_id = $sp[1].":".$sp[2].":".$sp[3]; 
-	my $bc4 = $sp[4];
-	my $umi	= $sp[5];
+	my $cell_id = $sp[-5].":".$sp[-4].":".$sp[-3]; 
+	my $bc4 = $sp[-2];
+	my $umi	= $sp[-1];
 #	my $sample = "sample_".$bc4;
 #	my $lib_type = substr($name[1],0,1)."NA";
 #                  foo<-foo[,.(chr,start,end,read.id,bc4,umi,cellid,sub.lib, sample,lib.type)]
